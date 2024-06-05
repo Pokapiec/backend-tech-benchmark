565931580dd6787fafc0acc4219c7b2b38033ff8
Pokapiec <pokapiec@gmail.com>
2024-06-05 21:13:43 +0200

    add setup check script

--- elixir_cowboy/mix.exs
+++ elixir_cowboy/mix.exs
@@ -22,12 +22,21 @@ defmodule ElixirCowboy.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug_cowboy, "~> 2.7.1"},
-      {:jason, "~> 1.4.1"},
-      {:ecto_sql, "~> 3.11.2"},
-      {:postgrex, ">= 0.18.0"}
+      {:plug_cowboy, "~> 2.5"},
+      {:jason, "~> 1.4"},
+      {:ecto_sql, "~> 3.2"},
+      {:postgrex, "~> 0.15"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp releases do
+    [
+      elixir_cowboy: [
+        include_executables_for: [:unix],
+        applications: [runtime_tools: :permanent]
+      ]
     ]
   end
 end
